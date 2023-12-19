8282086262a993e56092bb6848c6c8d76a9c0807
Suraj Shrestha <syuraj@gmail.com>
2023-12-18 19:16:17 -0500

    use same grok pattern in logstash as filebeat

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-freqtrade download-data --timeframes 1m 5m 15m 1h --config ./user_data/configs_backtest/config.json --timerange 20190101-20231201
+freqtrade download-data --timeframes 1m 5m 15m 1h --config ./user_data/configs_backtest/config.json --timerange 20231001-20231201
